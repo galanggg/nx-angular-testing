a8fd90ae4e71e3a9b7830d10b420d644a491c4c1
galanggg <tugasgalang@gmail.com>
2021-11-06 16:43:15 +0800

    initial commit

---
+++ apps/angular-nx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-angular-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-nx';
+}
